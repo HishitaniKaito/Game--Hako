5e383ebbf921cd074e034080507a71bba1f9433c
KAITO <90747002+HishitaniKaito@users.noreply.github.com>
2022-10-15 11:49:51 +0900

    worpとworppointを別々にした。tatewallとyokowallに長めのやつを追加 それぞれのシーンへ移動できるように、GameScene以外にボタンを追加、説明のボタン、ゲーム終了のボタンの追加

--- Assets/Script/SceneChange.cs
+++ Assets/Script/SceneChange.cs
@@ -10,4 +10,12 @@ public class SceneChange : MonoBehaviour
     {
         SceneManager.LoadScene(scenename);
     }
+    public void End()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+#else
+    Application.Quit();//ゲームプレイ終了
+#endif
+    }
 }
