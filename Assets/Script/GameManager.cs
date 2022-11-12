133c424c511f204ed66f2efbff6e3cb61c7377de
KAITO <90747002+HishitaniKaito@users.noreply.github.com>
2022-11-12 12:37:15 +0900

    Playerの変更、フォントの追加

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]float _timer = 0.0f;
+    [SerializeField] TMP_Text _timerText;
+    [SerializeField] TMP_Text _hpText;
+    [SerializeField] Player _player;
+    int _minits = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        if(_timer >= 60 )
+        {
+            _timer -= 60;
+            _minits++;
+        }
+        _timerText.text = $"Œo‰ßŽžŠÔ:{_minits:00}•ª{_timer:00}•b";
+    }
+}
